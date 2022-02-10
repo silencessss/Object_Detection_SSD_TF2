0adeba437cdd04cf21c7fc5e8728e10676a9d28e
ChanHT <chanhts323@gmail.com>
2022-02-10 20:20:25 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,76 @@
+import tensorflow as tf
+import cv2
+import os
+
+from configuration import OBJECT_CLASSES, save_model_dir, test_picture_dir, NUM_CLASSES
+from core.inference import InferenceProcedure
+from core.ssd import SSD
+from utils.tools import preprocess_image, resize_box
+
+
+def find_class_name(class_id):
+    id2class = dict((v, k) for k, v in OBJECT_CLASSES.items())
+    return id2class[class_id]
+
+
+# shape of boxes : (N, 4)  (xmin, ymin, xmax, ymax)
+# shape of scores : (N,)
+# shape of classes : (N,)
+def draw_boxes_on_image(image, boxes, scores, classes):
+    num_boxes = boxes.shape[0]
+    for i in range(num_boxes):
+        class_and_score = str(find_class_name(classes[i])) + ": " + str(scores[i])
+        cv2.rectangle(img=image, pt1=(boxes[i, 0], boxes[i, 1]), pt2=(boxes[i, 2], boxes[i, 3]), color=(255, 0, 0),
+                      thickness=2)
+        cv2.putText(img=image, text=class_and_score, org=(boxes[i, 0], boxes[i, 1] - 10),
+                    fontFace=cv2.FONT_HERSHEY_COMPLEX, fontScale=1.5, color=(0, 255, 255), thickness=2)
+    return image
+
+
+def test_single_picture(picture_dir, model):
+    image_tensor = preprocess_image(picture_dir)
+    image_tensor = tf.expand_dims(image_tensor, axis=0)
+    image_array = cv2.imread(picture_dir)
+    h, w, _ = image_array.shape
+    procedure = InferenceProcedure(model=model, num_classes=NUM_CLASSES)
+    results = procedure(image_tensor)
+    results = tf.squeeze(results, axis=0)
+    # 選擇置信度高於某一閥值的框
+    filter_mask = results[:, :, 0] > 0.6
+    filter_mask = tf.expand_dims(filter_mask, axis=-1)
+    filter_mask = tf.broadcast_to(filter_mask, shape=results.shape)
+    results = tf.boolean_mask(results, filter_mask)
+    # print("results: ", results)
+    results = tf.reshape(results, shape=(-1, 6))
+
+    scores = results[:, 0].numpy()
+    boxes = results[:, 1: 5].numpy()
+    boxes = resize_box(boxes, h, w)
+    classes = tf.cast(results[:, -1], dtype=tf.int32).numpy()
+    # print("Detection result：boxes: {}, classes: {}, scores: {}".format(boxes, classes, scores))
+
+    image_with_boxes = draw_boxes_on_image(image_array, boxes, scores, classes)
+
+    return image_with_boxes
+
+
+if __name__ == '__main__':
+    gpus = tf.config.list_physical_devices('GPU')
+    if gpus:
+        try:
+            for gpu in gpus:
+                tf.config.experimental.set_memory_growth(gpu, True)
+            logical_gpus = tf.config.list_logical_devices('GPU')
+            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+        except RuntimeError as e:
+            print(e)
+
+    ssd_model = SSD()
+    # 始终載入最新的權重文件
+    last_epoch = os.listdir(save_model_dir)[-2].split(".")[0]
+    ssd_model.load_weights(filepath=save_model_dir + last_epoch)
+    image = test_single_picture(picture_dir=test_picture_dir, model=ssd_model)
+
+    cv2.namedWindow("detect result", flags=cv2.WINDOW_NORMAL)
+    cv2.imshow("detect result", image)
+    cv2.waitKey(0)
---
+++ train.py
@@ -0,0 +1,96 @@
+import tensorflow as tf
+import time
+
+from configuration import IMAGE_HEIGHT, IMAGE_WIDTH, CHANNELS, EPOCHS, NUM_CLASSES, BATCH_SIZE, save_model_dir, \
+    load_weights_from_epoch, save_frequency, test_images_during_training, \
+    test_images_dir_list
+from core.ground_truth import ReadDataset
+from core.loss import MultiBoxLoss
+from core.make_dataset import TFDataset
+from core.ssd import SSD
+
+
+from utils.visualize import visualize_training_results
+
+
+def print_model_summary(network):
+    sample_inputs = tf.random.normal(shape=(1, IMAGE_HEIGHT, IMAGE_WIDTH, CHANNELS))
+    _ = network(sample_inputs, training=True)
+    network.summary()
+
+
+def main():
+    dataset = TFDataset()
+    train_data, train_count = dataset.generate_datatset()
+
+    model = SSD()
+    print_model_summary(model)
+
+    if load_weights_from_epoch >= 0:
+        model.load_weights(filepath=save_model_dir + "epoch-{}".format(load_weights_from_epoch))
+        print("成功從epoch-{}載入模型權重！".format(load_weights_from_epoch))
+
+    loss_fn = MultiBoxLoss(num_classes=NUM_CLASSES, overlap_thresh=0.5, neg_pos=3)
+
+    # optimizer
+    lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(initial_learning_rate=1e-3,
+                                                                 decay_steps=20000,
+                                                                 decay_rate=0.96)
+    optimizer = tf.optimizers.Adam(learning_rate=lr_schedule)
+
+    loss_metric = tf.metrics.Mean()
+    cls_loss_metric = tf.metrics.Mean()
+    reg_loss_metric = tf.metrics.Mean()
+
+    for epoch in range(load_weights_from_epoch + 1, EPOCHS):
+        start_time = time.time()
+        for step, batch_data in enumerate(train_data):
+            images, labels = ReadDataset().read(batch_data)
+
+            with tf.GradientTape() as tape:
+                predictions = model(images, training=True)
+                loss_l, loss_c = loss_fn(y_true=labels, y_pred=predictions)
+                total_loss = loss_l + loss_c
+            gradients = tape.gradient(total_loss, model.trainable_variables)
+            optimizer.apply_gradients(grads_and_vars=zip(gradients, model.trainable_variables))
+            loss_metric.update_state(values=total_loss)
+            cls_loss_metric.update_state(values=loss_c)
+            reg_loss_metric.update_state(values=loss_l)
+
+            time_per_step = (time.time() - start_time) / (step + 1)
+            print("Epoch: {}/{}, step: {}/{}, speed: {:.2f}s/step, loss: {:.10f}, "
+                  "cls loss: {:.10f}, reg loss: {:.10f}".format(epoch,
+                                                                EPOCHS,
+                                                                step,
+                                                                tf.math.ceil(train_count / BATCH_SIZE),
+                                                                time_per_step,
+                                                                loss_metric.result(),
+                                                                cls_loss_metric.result(),
+                                                                reg_loss_metric.result()))
+        loss_metric.reset_states()
+        cls_loss_metric.reset_states()
+        reg_loss_metric.reset_states()
+
+        if epoch % save_frequency == 0:
+            model.save_weights(filepath=save_model_dir + "epoch-{}".format(epoch), save_format="tf")
+
+        if test_images_during_training:
+            visualize_training_results(pictures=test_images_dir_list, model=model, epoch=epoch)
+
+    model.save_weights(filepath=save_model_dir + "epoch-{}".format(EPOCHS), save_format="tf")
+
+
+if __name__ == '__main__':
+    print("TensoFlow版本：{}".format(tf.__version__))
+    gpus = tf.config.list_physical_devices('GPU')
+    if gpus:
+        try:
+            for gpu in gpus:
+                tf.config.experimental.set_memory_growth(gpu, True)
+            logical_gpus = tf.config.list_logical_devices('GPU')
+            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+        except RuntimeError as e:
+            print(e)
+    main()
+
+
